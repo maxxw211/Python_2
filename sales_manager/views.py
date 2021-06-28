1f85691c983a449cb3e7afa6b536de71b4920b3f
maxxw211 <84463565+maxxw211@users.noreply.github.com>
2021-06-29 00:08:17 +0300

    Update views.py

--- sales_manager/views.py
+++ sales_manager/views.py
@@ -193,4 +193,4 @@ class AddRateBookAPI(APIView):
         )
         book.avg_rate = book.rated_user.aggregate(rate=Avg("rate"))["rate"]
         book.save(update_fields=["avg_rate"])
-        return Response({"avg_rate": book.avg_rate}, status=status.HTTP_201_CREATED)
+        return Response({"avg_rate": book.avg_rate}, status=status.HTTP_500)

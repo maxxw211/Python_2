a702d44b8877ef82d61dc0ed4f37ee8a2e57b01f
maxxw211 <84463565+maxxw211@users.noreply.github.com>
2021-07-14 00:28:15 +0300

    Update urls.py

--- books_shop/urls.py
+++ books_shop/urls.py
@@ -24,4 +24,4 @@ urlpatterns = [
     path('shop/', include('sales_manager.urls')),
     path('accounts/', include('sales_manager.urls')),
     path('__debug__/', include(debug_toolbar.urls)),
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+] # + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
